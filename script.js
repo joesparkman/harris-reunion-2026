e5c4cea50fbfa81ac410154b698c46ffba50d505
joesparkman <106938486+joesparkman@users.noreply.github.com>
2025-09-09 21:00:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Simple smooth scroll for on-page links
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', e => {
+    const id = a.getAttribute('href');
+    if(id.length>1){
+      e.preventDefault();
+      document.querySelector(id).scrollIntoView({behavior:'smooth'});
+    }
+  });
+});
